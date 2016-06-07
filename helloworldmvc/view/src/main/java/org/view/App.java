a2f83349fe8c55e9aa5dcce28bd11444b2a771cb
letis <letis@PC-B-PRIEUR>
2016-06-07 09:44:33 +0200

    Phase 2

---
+++ helloworldmvc/view/src/main/java/org/view/App.java
@@ -0,0 +1,13 @@
+package org.view;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
